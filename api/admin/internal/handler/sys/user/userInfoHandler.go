339c1ed82a0c314154f62866e79f02c30ce888ef
ZhaoJun-hz <zhaojuncodeing@163.com>
2024-11-25 17:30:20 +0800

    修改代码

--- api/admin/internal/handler/sys/user/userInfoHandler.go
+++ api/admin/internal/handler/sys/user/userInfoHandler.go
@@ -6,12 +6,20 @@ import (
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"ordering-platform/api/admin/internal/logic/sys/user"
 	"ordering-platform/api/admin/internal/svc"
+	"ordering-platform/api/admin/internal/types"
 )
 
+// user详情
 func UserInfoHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserInfoReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
 		l := user.NewUserInfoLogic(r.Context(), svcCtx)
-		resp, err := l.UserInfo()
+		resp, err := l.UserInfo(&req)
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
